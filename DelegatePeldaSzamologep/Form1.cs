f0df4599974eb4e770e1b6965d8242a5c02ecea8
exylaci <exy@freemail.hu>
2025-09-28 18:43:06 +0200

    delegate pelda (Magát a végrehajtandó műveltet adjuk át nem pedig a számítás eredményét, így azt csak később számoltatjuk ki.)

---
+++ DelegatePeldaSzamologep/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelegatePeldaSzamologep
+{
+    public partial class Form1 : Form
+    {
+        Muvelet Kivalasztott;
+
+        public Form1()
+        {
+            InitializeComponent();
+            cmbMuveletKivalaszto.DataSource = Enum.GetValues(typeof(MuveletekFunkciok));
+        }
+
+        private void cmbMuveletKivalaszto_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch ((MuveletekFunkciok)cmbMuveletKivalaszto.SelectedIndex)
+            {
+                case MuveletekFunkciok.Osszead:
+                    Kivalasztott = Muveletek.Osszead;
+                    break;
+                case MuveletekFunkciok.Kivon:
+                    Kivalasztott = Muveletek.Kivon;
+                    break;
+                case MuveletekFunkciok.Szoroz:
+                    Kivalasztott = Muveletek.Szoroz;
+                    break;
+                case MuveletekFunkciok.Oszt:
+                    Kivalasztott = Muveletek.Oszt;
+                    break;
+            }
+        }
+        private void btnVegrehajtas_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"A művelet eredménye: {Kivalasztott((double)numericUpDown1.Value, (double)numericUpDown2.Value)}", "Eredmény:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ DelegatePeldaSzamologep/Muveletek.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatePeldaSzamologep
+{
+    public enum MuveletekFunkciok
+    {
+        Osszead,
+        Kivon,
+        Szoroz,
+        Oszt
+    }
+
+    delegate double Muvelet(double szam1, double szam2);
+
+    internal class Muveletek
+    {
+        public static double Osszead(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Kivon(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Szoroz(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Oszt(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
