fdb439fc6105cb0f4e315f190f31e05939d6a0cc
Exner Laszlo <exy@freemail.hu>
2025-10-26 22:00:51 +0100

    Összefoglaló feladat (C# alapok, objektumok, ablakozás, fájlkezelés, osztályok, listek) - init

---
+++ AblakFileOsztalyListaGyakorlasJarmuJavitoMuhely/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AblakFileOsztalyListaGyakorlasJarmuJavitoMuhely")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AblakFileOsztalyListaGyakorlasJarmuJavitoMuhely")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("19df9590-ad6a-43c4-8267-ff8a3dcb216f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
