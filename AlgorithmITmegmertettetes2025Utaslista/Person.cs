2907ef9cea73765b357819725a8e8d990b1aabbe
Exner Laszlo <exy@freemail.hu>
2025-11-05 10:18:05 +0100

    IT megmérettetés 2025 (Utaslista) - solution

---
+++ AlgorithmITmegmertettetes2025Utaslista/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmITmegmertettetes2025Utaslista
+{
+    public class Person
+    {
+        public string VezetekNev { get; set; }
+        public string KeresztNev { get; set; }
+        public DateTime Szuletett { get; set; }
+        public string AnyjaVezetekNeve { get; set; }
+        public string AnyjaKeresztNeve { get; set; }
+        public List<Person> children = new List<Person>();
+
+        public string Neve() { return VezetekNev + " " + KeresztNev; }
+        public string AnyjaNeve() { return AnyjaVezetekNeve + " " + AnyjaKeresztNeve; }
+
+        public Person(string personString)
+        {
+            string[] parts = personString.Split(',');
+            if (parts.Length != 3)
+            {
+                throw new InvalidDataException($"Hibás formátum: {personString}");
+            }
+
+            var nameParts = parts[0].Split(' ', 2, StringSplitOptions.RemoveEmptyEntries);
+            if (nameParts.Length == 2)
+            {
+                VezetekNev = nameParts[0];
+                KeresztNev = nameParts[1];
+            }
+            else
+            {
+                VezetekNev = nameParts[0];
+                KeresztNev = string.Empty;
+            }
+
+            string[] formatumok = { "yyyy.M.d.", "yyyy.MM.dd." };
+            Szuletett = DateTime.ParseExact(parts[1].Trim(), formatumok, CultureInfo.InvariantCulture, DateTimeStyles.None);
+
+            nameParts = parts[2].Split(' ', 2, StringSplitOptions.RemoveEmptyEntries);
+            if (nameParts.Length == 2)
+            {
+                AnyjaVezetekNeve = nameParts[0];
+                AnyjaKeresztNeve = nameParts[1];
+            }
+            else
+            {
+                AnyjaVezetekNeve = nameParts[0];
+                AnyjaKeresztNeve = string.Empty;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Neve() + "," + Szuletett.Year + "." + Szuletett.Month + "." + Szuletett.Day + ".," + AnyjaNeve()
+                + ", children:" + children.Count();
+        }
+    }
+}
--- AlgorithmITmegmertettetes2025Utaslista/Program.cs
+++ AlgorithmITmegmertettetes2025Utaslista/Program.cs
@@ -1,10 +1,135 @@
 ﻿namespace AlgorithmITmegmertettetes2025Utaslista
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static List<Person> people = [];
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AdatokBetoltese("..\\..\\..\\utaslista.txt");
+            MaEsHolnapSzuletettekKerese();
+            GyerekekKigyujtese();
+            KinekVan10Unokaja();
+            KiKortars();
+        }
+
+        private static void AdatokBetoltese(string filePath)
+        {
+            try
+            {
+                foreach (var line in File.ReadLines(filePath))
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        Console.WriteLine("Üres sor.");
+                        continue;
+                    }
+                    people.Add(new Person(line));
+                }
+                Console.WriteLine("Beolvasott személyek száma: " + people.Count);
+                //Console.WriteLine("Beolvasott személyek:");
+                //foreach (var person in people)
+                //{
+                //    Console.WriteLine(person.ToString());
+                //}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hiba a fájl beolvasása közben: {ex.Message}");
+            }
+        }
+        private static void MaEsHolnapSzuletettekKerese()
+        {
+            DateTime ma = new DateTime(2025, 1, 1);
+            DateTime holnap = new DateTime(2025, 1, 1);
+            int macount = 0;
+            int holnapcount = 0;
+            for (int i = 0; i < 365; i++)
+            {
+                holnap = ma.AddDays(1);
+                foreach (var person in people)
+                {
+                    if (person.Szuletett.Month == ma.Month && person.Szuletett.Day == ma.Day)
+                    {
+                        macount++;
+                        if (macount > 1)
+                        {
+                            break;
+                        }
+                    }
+                    if (person.Szuletett.Month == holnap.Month && person.Szuletett.Day == holnap.Day)
+                    {
+                        holnapcount++;
+                        if (holnapcount > 4)
+                        {
+                            break;
+                        }
+                    }
+                }
+                if ((macount == 1) && (holnapcount == 4))
+                {
+                    Console.WriteLine($"Pontosan 1 ma született és 4 holnap született dátuma: {ma.Month}.{ma.Day}");
+                }
+                ma = ma.AddDays(1);
+                macount = 0;
+                holnapcount = 0;
+            }
+        }
+
+        private static void GyerekekKigyujtese()
+        {
+            foreach (var person in people)
+            {
+                foreach (var potentialChild in people)
+                {
+                    if (string.Equals(potentialChild.AnyjaNeve(), person.Neve()) && DateTime.Compare(potentialChild.Szuletett, person.Szuletett) > 0)
+                    {
+                        person.children.Add(potentialChild);
+                        //Console.WriteLine(person + " gyereke: " + potentialChild);
+                    }
+                }
+            }
+        }
+
+        private static void KinekVan10Unokaja()
+        {
+            int unokaszam = 0;
+            foreach (var person in people)
+            {
+                foreach (var child in person.children)
+                {
+                    unokaszam += child.children.Count;
+                }
+                if (unokaszam == 10)
+                {
+                    Console.WriteLine($"{person.Neve()}-nek van {unokaszam} unokája.");
+                }
+                unokaszam = 0;
+            }
+        }
+
+        private static void KiKortars()
+        {
+            int kortarszam = 0;
+            int maxkortarszam = 0;
+            Person legtobb = null;
+            foreach (var person in people)
+            {
+                foreach (var masik in people)
+                {
+                    if (person != masik && Math.Abs((person.Szuletett - masik.Szuletett).TotalDays) < 730)
+                    {
+                        ++kortarszam;
+                    }
+                }
+                if (kortarszam > maxkortarszam)
+                {
+                    maxkortarszam = kortarszam;
+                    legtobb = person;
+                }
+                kortarszam = 0;
+            }
+            Console.WriteLine($"{legtobb.Neve()}-nek van a legtöbb kortársa: {maxkortarszam} fő.");
         }
     }
 }
