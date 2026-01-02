5c509b79e6afb4257e1dc8c9dcb89c0bd4e7ea74
Exner Laszlo <exy@freemail.hu>
2026-01-02 10:36:52 +0100

    Aszinkron műveletek példák

---
+++ Asszinkronitas/Demo01_AsyncAwait.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asszinkronitas
+{
+    public static class Demo01_AsyncAwait
+    {
+        public static async Task RunAsync()
+        {
+            Console.WriteLine("Demo 1 - Konkurencia (nem feltétlenül párhuzamos): _AsyncAwait started.");
+
+            Stopwatch stopwatch = Stopwatch.StartNew();                      // Start measuring time
+
+            Task t1 = SimulateLongRunningOperationAsync("A", 1000);          // Start the asynchronous operation without awaiting
+            Task t2 = SimulateLongRunningOperationAsync("B", 2000);          // Start the asynchronous operation without awaiting
+            Task t3 = SimulateLongRunningOperationAsync("C", 800);           // Start the asynchronous operation without awaiting
+
+
+            await Task.WhenAll(t1, t2, t3);                                  // Call the asynchronous methods
+
+            Console.WriteLine($"Result: All operations completed.");
+            stopwatch.Stop();
+            Console.WriteLine($"Elapsed Time: {stopwatch.ElapsedMilliseconds} ms");
+            Console.WriteLine("Demo01_AsyncAwait completed.");
+        }
+        private static async Task SimulateLongRunningOperationAsync(string name, int delayMS)
+        {
+            Console.WriteLine($"Starting operation:  {name} Thread ID: {Environment.CurrentManagedThreadId}");
+            await Task.Delay(delayMS);                                      // Simulate a long-running operation with x seconds delay
+            Console.WriteLine($"Completed operation: {name} Thread ID: {Environment.CurrentManagedThreadId}");
+
+            return;
+        }
+    }
+}
---
+++ Asszinkronitas/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asszinkronitas
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Menu();
+                try
+                {
+                    switch (Console.ReadKey().KeyChar.ToString().ToLower())
+                    {
+                        case "1":
+                            await Demo01_AsyncAwait.RunAsync();
+                            break;
+                        case "q":
+                            return;
+                        default:
+                            Console.WriteLine("Érvénytelen választás.");
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+            }
+        }
+
+        private static void Menu()
+        {
+            Console.WriteLine("\n1 - Concurency ASYNC/AWAIT Task delay-jel (IO/Bound)");
+            Console.WriteLine("\nQ - Kilépés");
+            Console.WriteLine("--------------------------------------------------");
+            Console.Write("Válassz! ");
+
+        }
+    }
+}
