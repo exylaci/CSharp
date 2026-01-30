2ab85aeadc7138ae424b1f25e01a5fe24289ea04
Exner Laszlo <exy@freemail.hu>
2026-01-30 07:49:36 +0100

    GDI grafikus objektum alakzatok

---
+++ GrafikusObjektumAlap/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace GrafikusObjektumAlap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canavas = e.Graphics;
+            canavas.DrawRectangle(new Pen(Color.Blue), 50, 50, 100, 50);
+            canavas.FillEllipse(new Pen(Color.Orange).Brush, 100, 100, 60, 60);
+            canavas.FillRectangle(new Pen(Color.Chartreuse).Brush, 150, 150, 100, 50);
+        }
+    }
+}
