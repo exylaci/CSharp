87f4b3e2161785df111c305ebb404370f1e6a623
Exner Laszlo <exy@freemail.hu>
2026-01-31 10:58:21 +0100

    Vizsga - osztályok

---
+++ VizsgaMunkafolyamatok/Munkafolyamat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace VizsgaMunkafolyamatok
+{
+    internal class Munkafolyamat
+    {
+        private string megnevezes;
+        private double ar;
+
+        public string Megnevezes
+        {
+            get => megnevezes;
+            private set => megnevezes = (value.Length > 0) ? value : throw new ArgumentException("Megnevezés nem lehet üres!");
+        }
+        public double Ar
+        {
+            get => ar;
+            private set => ar = (ar > 0) ? value : throw new ArgumentException("Árnak pozitív számnak kell lennie!");
+        }
+
+
+        public Munkafolyamat(string megnevezes, double ar)
+        {
+            this.megnevezes = megnevezes;
+            this.ar = ar;
+        }
+
+        public double BruttoAr()
+        {
+            return Ar * 1.27;
+        }
+    }
+}
---
+++ VizsgaMunkafolyamatok/Szemely.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VizsgaMunkafolyamatok
+{
+    internal class Szemely
+    {
+        private string nev;
+        private string lakcim;
+        private string telefonszam;
+
+        public string Nev
+        {
+            get => nev;
+            private set => nev = (value.Length > 0) ? value : throw new ArgumentException("Név nem lehet üres!");
+        }
+        public string Lakcim
+        {
+            get => lakcim;
+            private set => lakcim = (value.Length > 0) ? value : throw new ArgumentException("Lakcim nem lehet üres!");
+        }
+        public string Telefonszam
+        {
+            get => telefonszam;
+            private set => telefonszam = (value.Length > 0) ? value : throw new ArgumentException("Telefonszam nem lehet üres!");
+        }
+
+
+        public Szemely(string nev, string lakcim, string telefonszam)
+        {
+            this.nev = nev;
+            this.lakcim = lakcim;
+            this.telefonszam = telefonszam;
+        }
+    }
+}
