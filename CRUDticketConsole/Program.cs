67a5a0699386292c8ed251b23646f88c206150f0
Exner Laszlo <exy@freemail.hu>
2026-03-29 10:45:35 +0200

    CRUD parancsok kiadása konzol ablakból - initial

---
+++ CRUDticketConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+
+namespace CRUDticketConsole;
+
+internal static class Program
+{
+    static async Task Main()
+    {
+        string url = "https://localhost:7210/";
+        var client = new HttpClient(new HttpClientHandler()
+        {
+            ServerCertificateCustomValidationCallback =
+                HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        });
+
+        var response = await client.GetAsync(url + "api/Tickets");
+
+        Console.WriteLine($"{(int)response.StatusCode}\t{response.StatusCode}");
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+
+        JsonDocument tickets = JsonDocument.Parse(responseBody);
+
+        foreach (JsonElement ticket in tickets.RootElement.EnumerateArray())
+        {
+            foreach (JsonProperty tag in ticket.EnumerateObject())
+            {
+                Console.WriteLine($"{tag.Name,15}: {tag.Value}");
+            }
+
+            Console.WriteLine("----------------------------------------------");
+        }
+
+        Console.ReadKey();
+    }
+}
